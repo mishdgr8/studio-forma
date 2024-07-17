d54e1f78cd5ed3f4f6dccf5f1be37bccb2a3564b
mishdgr8 <Mishaelmordi@gmail.com>
2024-07-17 16:53:33 +0100

    Working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Hero from "@/../public/Images/heroImage.jpg";
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"], // Specify the weights you need
+});
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  items-center justify-between p-24">
-      <div className=" w-full z-10 max-w-5xl items-center justify-center   lg:flex">
-        <div className="text-8xl">STUDIO FORMA 96</div>
+    <main className="flex min-h-screen flex-col  items-center justify-between pt-24">
+      <div className="  z-10 max-w-screen items-center justify-between   lg:flex">
+        <section className="hero">
+          <div className="flex-col items-center justify-around">
+            <div
+              className={`text-9xl flex justify-center ${lato.className} mb-8`}
+            >
+              STUDIO FORMA 96
+            </div>
+            <div className="hero-video h-44 w-full">
+              <Image alt="HERO IMAGE" src={Hero} className="px-2" />
+            </div>
+          </div>
+        </section>
       </div>
     </main>
   );
