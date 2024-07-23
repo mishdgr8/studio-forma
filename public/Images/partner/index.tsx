38e8d5e523a90b11bed3b7b9adc64e3f26e11602
mishdgr8 <Mishaelmordi@gmail.com>
2024-07-23 14:04:06 +0100

    refactored code and made partner slider a component

---
+++ public/Images/partner/index.tsx
@@ -0,0 +1,34 @@
+// "use client";
+
+// import React from "react";
+// import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import Flexbox from "../partner/flexform-logo-1.png";
+// import Poliform from "../partner/poliform-logo.png";
+// import Rima from "../partner/logo-rimadesio-1536x259.png";
+// import Siematics from "../partner/siematic-logo.png";
+// import Occhio from "../partner/occhio-logo-1536x439.png";
+// import Dedon from "../partner/dedon-logo-yellow.svg";
+// import Image from "next/image";
+// const PartnerImage = () => {
+//   const images = [
+//     { src: Flexbox, alt: "Flexform" },
+//     { src: Poliform, alt: "Poliform" },
+//     { src: Rima, alt: "Rimadesio" },
+//     { src: Siematics, alt: "Siematic" },
+//     { src: Occhio, alt: "Occhio" },
+//     { src: Dedon, alt: "Dedon" },
+//   ];
+
+//   return (
+//     <Carousel showThumbs={false} infiniteLoop autoPlay>
+//       {images.map((image, index) => (
+//         <div key={index}>
+//           <Image src={image.src} alt={image.alt} />
+//         </div>
+//       ))}
+//     </Carousel>
+//   );
+// };
+
+// export default PartnerImage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,92 @@
-import Navbar from "@/components/Navbar";
+"use client";
+import { useState, useRef } from "react";
 import Image from "next/image";
-import Hero from "@/../public/Images/heroImage.jpg";
+import Link from "next/link";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
+import Flexbox from "../../public/Images/partner/flexform-logo-1.png";
+import Poliform from "../../public/Images/partner/poliform-logo.png";
+import Rima from "../../public/Images/partner/logo-rimadesio-1536x259.png";
+import Siematics from "../../public/Images/partner/siematic-logo.png";
+import Occhio from "../../public/Images/partner/occhio-logo-1536x439.png";
+import Dedon from "../../public/Images/partner/dedon-logo-yellow.svg";
 import { Lato } from "next/font/google";
 import "./../app/globals.css";
-import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa6";
+import { FaArrowCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const lato = Lato({
   subsets: ["latin"],
   weight: ["400", "700"], // Specify the weights you need
 });
 
+const PartnerSlider = () => {
+  const images = [
+    Flexbox,
+    Poliform,
+    Rima,
+    Siematics,
+    Occhio,
+    Dedon,
+    Flexbox,
+    Poliform,
+    Rima,
+    Siematics,
+    Occhio,
+    Dedon,
+  ];
+  const sliderRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      const imageWidth = sliderRef.current.querySelector("div").offsetWidth + 8; // Including margin
+      sliderRef.current.scrollBy({ left: -imageWidth, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      const imageWidth = sliderRef.current.querySelector("div").offsetWidth + 8; // Including margin
+      sliderRef.current.scrollBy({ left: imageWidth, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="partner flex flex-col w-full pl-[4rem] mb-[8rem]">
+      <h2 className="text-2xl">Brands we work with</h2>
+      <div className="relative mt-8">
+        <button
+          onClick={scrollLeft}
+          className="absolute text-white left-0 z-10 bg-black/20 p-2 rounded-full transform -translate-y-1/2 top-1/2"
+        >
+          <FaArrowLeft />
+        </button>
+        <div
+          className="flex overflow-x-scroll no-scrollbar"
+          ref={sliderRef}
+          style={{ scrollBehavior: "smooth" }}
+        >
+          {images.map((image, index) => (
+            <div key={index} className="mr-2 flex-shrink-0">
+              <div className="w-60 h-[10rem] flex items-center justify-center bg-white">
+                <Image className="w-28 h-5" src={image} alt="brand" />
+              </div>
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={scrollRight}
+          className="absolute text-white right-0 z-10 bg-black/20 p-2 rounded-full transform -translate-y-1/2 top-1/2"
+        >
+          <FaArrowRight />
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  items-center justify-between pt-24">
-      <div className=" flex-col z-10 max-w-screen items-center justify-between   lg:flex">
+    <main className="flex min-h-screen flex-col pt-24">
+      <div className="flex-col z-10 max-w-screen items-center justify-between lg:flex">
         <section className="hero h-auto">
           <div className="flex-col items-center justify-around">
             <div
@@ -64,20 +136,19 @@ export default function Home() {
         </section>
         <section className="my-[10rem] ml-[28rem]">
           <div className="">
-            <p className="text-3xl text-gray-600 leading-snug">
+            <p className="text-3xl leading-snug">
               We discreetly emphasize the luxury of the places you want to be.{" "}
               <br />
               Home, office, outdoor: our furniture and designs co-create spaces
               that support your well-being.
             </p>
             <Link href={"/"} className="text-xs">
-              <p className="mt-[4rem] text-gray-600"> Read more</p>
+              <p className="mt-[4rem]"> Read more</p>
             </Link>
           </div>
-          <section className="Brand-partners">
-            <div></div>
-          </section>
         </section>
+
+        <PartnerSlider />
       </div>
     </main>
   );
